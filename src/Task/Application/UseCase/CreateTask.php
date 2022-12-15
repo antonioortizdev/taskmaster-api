0e78b4cd88982f0810a0e2184f2524ce5040fee9
Antonio Ortiz <antonioortizdev@gmail.com>
2022-12-15 16:45:16 +0100

    refactor use case

--- src/Task/Application/UseCase/CreateTask.php
+++ src/Task/Application/UseCase/CreateTask.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace Src\Task\Application\UseCase;
+
 use Src\Task\Domain\Repository\TaskRepository;
 use Src\Task\Domain\Task;
 
-final class CreateTask
+class CreateTask
 {
     public function __construct(private TaskRepository $repository) {}
 
-    public function __invoke(array $newTaskData): void
+    public function __invoke(array $taskData): void
     {
-        $this->repository->save(Task::fromPrimitives($newTaskData));
+        $task = Task::fromPrimitives($taskData);
+        $this->repository->save($task);
     }
 }
